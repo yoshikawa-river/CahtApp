a9ccaf6859da472cf15d0ee4ac33c2213f6cceed
yoshikawa-river <yuttta0411@gmail.com>
2023-03-19 05:40:18 +0900

    add:envの読み込んだ情報からデータベースへの接続を行う処理を作成

--- cmd/chat/main.go
+++ cmd/chat/main.go
@@ -5,13 +5,19 @@ import (
 	"net/http"
 
 	adapterHttp "github.com/yoshikawa-river/CahtApp/adapter/http"
+	"github.com/yoshikawa-river/CahtApp/config"
+	"github.com/yoshikawa-river/CahtApp/infla"
 )
 
 func main() {
+	appConfig := config.LoadConfig()
+
 	router := adapterHttp.InitRouter()
 
+	infla.NewConnDB(appConfig.DBInfo)
+
 	server := http.Server{
-		Addr:    ":8080",
+		Addr:    ":" + appConfig.HttpInfo.PROTOCOL,
 		Handler: router,
 	}
 
