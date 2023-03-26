b1332add697a551fe7e48c46e757b1fad61a7d9c
yoshikawa-river <yuttta0411@gmail.com>
2023-03-27 00:43:12 +0900

    update:jsonを返す処理のresponse層への移動に伴う修正

--- adapter/http/health.go
+++ adapter/http/health.go
@@ -1,26 +1,18 @@
 package http
 
 import (
-	"encoding/json"
-	"fmt"
 	"net/http"
-	"os"
 
-	"github.com/yoshikawa-river/CahtApp/config"
-	"github.com/yoshikawa-river/CahtApp/infla"
+	"github.com/yoshikawa-river/ChatApp/adapter/http/response"
+	"github.com/yoshikawa-river/ChatApp/config"
+	"github.com/yoshikawa-river/ChatApp/infla"
 )
 
-type SimpleResponse struct {
-	Status  int    `json:"status"`
-	Message string `json:"message,omitempty"`
-	Detail  string `json:"string,omitempty"`
-}
-
 func ServerHealthCheck(w http.ResponseWriter, r *http.Request) {
-	rs := SimpleResponse{
+	rs := response.SimpleResponse{
 		Status: http.StatusOK,
 	}
-	respondJson(w, rs, http.StatusOK)
+	response.RespondJson(w, rs, http.StatusOK)
 }
 
 func DBHealthCheck(w http.ResponseWriter, r *http.Request) {
@@ -28,33 +20,23 @@ func DBHealthCheck(w http.ResponseWriter, r *http.Request) {
 	_, err := infla.NewConnDB(DBInfo)
 	if err != nil {
 		// DBへのコネクションにてエラーが発生した場合は503レスポンス
-		rs := SimpleResponse{
+		rs := response.SimpleResponse{
 			Status:  http.StatusServiceUnavailable,
 			Message: "failed to get connection database",
-			Detail:  err.Error(),
+			Error:   err.Error(),
 		}
-		respondJson(w, rs, rs.Status)
+		response.RespondJson(w, rs, rs.Status)
 		return
 	}
 
-	rs := SimpleResponse{
+	rs := response.SimpleResponse{
 		Status:  http.StatusOK,
 		Message: "success to connect server",
 	}
-	respondJson(w, rs, rs.Status)
+	response.RespondJson(w, rs, rs.Status)
 }
 
 func HealthCheck(w http.ResponseWriter, r *http.Request) {
 	ServerHealthCheck(w, r)
 	DBHealthCheck(w, r)
-}
-
-func respondJson(w http.ResponseWriter, body interface{}, status int) {
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	w.WriteHeader(status)
-	if err := json.NewEncoder(w).Encode(body); err != nil {
-		fmt.Fprintf(os.Stderr, "failed to encode response by error '%#v'", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
 }
