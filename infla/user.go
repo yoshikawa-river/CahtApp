d648c3340287121ba9d2afd536aff384a9f82075
yoshikawa-river <yuttta0411@gmail.com>
2023-03-27 00:48:01 +0900

    add:userのinfra層とcreateの処理、バリデーションを作成

---
+++ infla/user.go
@@ -0,0 +1,56 @@
+package infla
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"net/mail"
+
+	"github.com/volatiletech/sqlboiler/v4/boil"
+	"github.com/volatiletech/sqlboiler/v4/queries/qm"
+	"github.com/yoshikawa-river/ChatApp/domain/models"
+	"github.com/yoshikawa-river/ChatApp/domain/repository"
+)
+
+type UserRepository struct {
+	DB *sql.DB
+}
+
+func NewUserRepository(db *sql.DB) repository.IUserRepository {
+	return &UserRepository{DB: db}
+}
+
+func (ur *UserRepository) Create(ctx context.Context, u *models.User) (*models.User, error) {
+	if err := ur.Validate(ctx, u); err != nil {
+		return nil, err
+	}
+	return u, u.Insert(ctx, ur.DB, boil.Infer())
+}
+
+func (ur *UserRepository) Validate(ctx context.Context, u *models.User) error {
+	if len(u.Name) == 0 {
+		return errors.New("The name field is required.")
+	}
+
+	if len(u.Name) >= 30 {
+		return errors.New("The name must not be greater than 30")
+	}
+
+	if len(u.Email) == 0 {
+		return errors.New("The Email field is required.")
+	}
+
+	if len(u.Email) >= 319 {
+		return errors.New("The email must not be greater than 319")
+	}
+
+	if _, err := mail.ParseAddress(u.Email); err != nil {
+		return errors.New("The email must be a valid email address.")
+	}
+
+	if exists, _ := models.Users(qm.Where("email=?", u.Email)).Exists(ctx, ur.DB); exists {
+		return errors.New("The email has already been taken.")
+	}
+
+	return nil
+}
